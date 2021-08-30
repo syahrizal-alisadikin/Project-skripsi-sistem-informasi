58e69e85557129784254cabd2bbfa306fd4921ac
alisadikinsyahrizal <alisadikinsyahrizal@gmail.com>
2021-08-30 23:14:49 +0700

    update peserta

--- app/Http/Controllers/Admin/PesertaController.php
+++ app/Http/Controllers/Admin/PesertaController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Peserta;
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class PesertaController extends Controller
 {
-    //
+    public function index()
+    {
+        $peserta = Transaction::with('peserta')->where('status','accept')->get();
+        return view('pages.admin.peserta.index',compact('peserta'));
+    }
 }
--- app/Transaction.php
+++ app/Transaction.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = ["peserta_id",'status','total_harga'];
+
+    public function peserta()
+    {
+        return $this->belongsTo(Peserta::class,'id','peserta_id');
+    }
 }
