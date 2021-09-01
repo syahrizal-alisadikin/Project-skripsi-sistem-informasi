5b01a43f800519095900e926c5cb8ac527ce163f
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2021-09-01 21:33:05 +0700

    Fronte End

---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Store - Your Best Marketplace</title>
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link href="{{url('style/main.css')}}" rel="stylesheet" />
+  </head>
+
+  <body>
+    <nav
+      class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
+      data-aos="fade-down"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="/index.html">
+          <div class="card bg-dark" style="width: 2rem; height: 2rem;">
+          </div>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html"
+                >Beranda <span class="sr-only">(current)</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/categories.html">Tutorial</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Jadwal</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Perbaikan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!--- Content --->
+    @yield('content')
+
+    <footer class="mt-5">
+      <div class="container">
+        <div class="row mb-auto">
+          <div class="col-12 text-center">
+            <div class="pt-5 pb-5">
+              <p><i>2021 Copyright Store Apache Surf</i></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{url('/vendor/jquery/jquery.slim.min.js')}}"></script>
+    <script src="{{url('/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <script src="{{url('script/navbar-scroll.js')}}"></script>
+  </body>
+</html>
---
+++ resources/views/pages/user/home.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.user', ['title' => 'Apache Surf'])
+
+@section('content')
+    <div class="page-content">
+      <section class="hero">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12">
+              <h1>Selamat <b>Datang!</b> <br> di <b>Apache</b> Surf Club</h1>
+              <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
+              <a class="btn btn-started mt-4 mb-auto" href="">Get Started</a>
+            </div>
+            <div class="col-lg-3 col-md-5 col-sm-12 col-xs-12 ml-auto">
+              <img class="img-fluid rounded" src="https://picsum.photos/250">
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="class-surf my-4">
+        <div class="container">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-12 mx-auto text-center" data-aos="fade-up">
+                <h2 class="font-weight-bold">Kelas Selancar Air</h2>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div class="component-products ">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url(https://picsum.photos/250)"
+                  ></div>
+                </div>
+                  <div class="products-text">Beginner</div>
+                  <a class="btn btn-join p-1 mt-1" href="#">Join
+                  </a>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div class="component-products ">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url(https://picsum.photos/250)"
+                  ></div>
+                </div>
+                  <div class="products-text">Beginner</div>
+                  <a class="btn btn-join p-1 mt-1" href="#">Join
+                  </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="store-new-products">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="card bg-success text-white">
+                <div class="card-body">
+                  <h2 class="text-center font-weight-bold">Welcome</h2>
+                  <h2 class="text-center font-weight-bold">Apache Surf Club</h2>
+                  <div class="col-lg-11 mx-auto mt-4" style="line-height:1.5rem;">
+                  <p class="text-justify">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat quo qui at ut molestiae totam voluptates accusamus sapiente vero, fugit dolorum aliquam nesciunt omnis laborum recusandae eveniet. Excepturi, ea.
+                    Eius, sit at libero quae nostrum earum repellat error animi. Veniam dignissimos obcaecati inventore vitae at, assumenda cupiditate officiis ad nisi temporibus quaerat culpa ea numquam necessitatibus libero. Quo, ad?
+                    Soluta perspiciatis totam reprehenderit ad explicabo culpa ducimus quis eaque provident consequuntur! Neque optio iure, esse eum recusandae illum nisi harum excepturi id aliquid distinctio doloremque tempora asperiores atque amet?
+                    Suscipit eos commodi fuga est iste? Minus et cupiditate enim quae quia, iusto iste voluptate cum placeat aut fugit quaerat amet sit officiis hic possimus rerum nostrum sapiente dolor tempora!
+                    Ipsam consequuntur quia ipsum laborum nulla quidem repellendus, facere at ullam harum sit architecto culpa possimus illum? Possimus nihil facere vitae doloremque illo est, ratione quos beatae nostrum assumenda voluptate.
+                </p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+@endsection
