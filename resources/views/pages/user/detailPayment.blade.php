3bf2abb51c51d53aaa8b55339f47a12920e6e913
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2021-09-11 23:15:31 +0700

    Update detailPayment.blade.php
    Update Detail Payment

--- resources/views/pages/user/detailPayment.blade.php
+++ resources/views/pages/user/detailPayment.blade.php
@@ -39,25 +39,7 @@
                   <p>Total Biaya</p>
                   <p>{{ moneyFormat($peserta->transaction->total_harga) }}</p>
                 </div>
-                <div class="form-inline justify-content-between">
-                  <p>Nama Lengkap</p>
-                  <p>{{ $peserta->user->name }}</p>
-                </div>
-                <div class="form-inline justify-content-between">
-                  <p>No hp</p>
-                  <p>{{ $peserta->user->phone }}</p>
-                </div>
-                <div class="form-inline justify-content-between">
-                  <p>E-mail</p>
-                  <p>{{ $peserta->user->email }}</p>
-                </div>
-              </div>
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 mx-auto">
-                <div class="form-inline justify-content-between">
-                  <p>Nama</p>
-                  <p>{{ $peserta->user->name }}</p>
-                </div>
-                <div class="form-inline justify-content-between">
+                 <div class="form-inline justify-content-between">
                   <p>Status Pembayaran</p>
                 </div>
                 <div
@@ -74,33 +56,49 @@
                   <small>Segera melakukan pembayaran</small>
                   @endif
                 </div>
-               
-                <div
-                  class="
-                    form-inline
-                    justify-content-between
-                    font-weight-bold
-                  "
-                >
-                  <p>DGWUIG1809484</p>
+                <div class="form-inline justify-content-between">
+                  <p>Nama Lengkap</p>
+                  <p>{{ $peserta->user->name }}</p>
                 </div>
                 <div class="form-inline justify-content-between">
-                  <p>Upload Pembayaran</p>
+                  <p>No hp</p>
+                  <p>{{ $peserta->user->phone }}</p>
                 </div>
                 <div class="form-inline justify-content-between">
-                  <input type="file" />
+                  <p>E-mail</p>
+                  <p>{{ $peserta->user->email }}</p>
                 </div>
-                <div class="component-products">
-                  <div class="mx-auto my-3 text-center ml-auto">
-                    <button class="btn btn-join w-100">Upload File</button>
+              </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-sm-3 mb-xs-3">
+                  <div class="component-products">
+                    <div class="mx-auto my-3 text-center ml-auto">
+                      <a href="{{ route('midtrans-callback')}}">
+                        <button class="btn btn-join w-100">Checkout</button>
+                      </a>
+                    </div>
                   </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-sm-3 mb-xs-3">
+                  <div class="component-products">
+                    <div class="mx-auto my-3 text-center ml-auto">
+                      <a href="{{ route('payment', $peserta->id)}}">
+                        <button class="btn btn-info w-100">Edit</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-sm-3 mb-xs-3">
                   <div class="mx-auto text-center ml-auto">
-                    <button
-                      class="btn btn-started w-100"
-                      style="background-color: #e7e6e6b4"
-                    >
-                      Cancel
-                    </button>
+                    <a href="{{ route('home')}}">
+                        <button
+                          class="btn btn-started w-100"
+                          style="background-color: #e7e6e6b4"
+                        >
+                          Cancel
+                        </button>
+                      </a>
                   </div>
                 </div>
               </div>
